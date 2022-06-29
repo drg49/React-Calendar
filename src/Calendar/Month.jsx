aab52b3fd1102a811cf089ae64588eedaba12fc2
Daniel Gavin <81581925+drg49@users.noreply.github.com>
2022-06-28 19:21:59 -0700

    Adding functionality to change the month

--- src/Calendar/Month.jsx
+++ src/Calendar/Month.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import * as utils from '../utils';
 
-export default function Month({ month }) {
+export default function Month() {
+  const [month, setMonth] = React.useState(utils.now.getMonth());
   
+  const increaseMonth = () => month === 11 ? setMonth(0) : setMonth(month + 1);
+
+  const decreaseMonth = () => month === 0 ? setMonth(11) : setMonth(month - 1);
+
   return (
     <div id="month">
-      <button class="btn">&#10094;</button>
-      <span>{month}</span>
-      <button class="btn">&#10095;</button>
+      <button
+        class="btn"
+        onClick={decreaseMonth}
+      >
+        &#10094;
+      </button>
+      <span>
+        {utils.displayMonth(month)}
+      </span>
+      <button
+        class="btn"
+        onClick={increaseMonth}
+      >
+        &#10095;
+      </button>
     </div>
   )
 }
--- src/Calendar/index.jsx
+++ src/Calendar/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Month from './Month';
-import * as utils from '../utils';
 
 import './index.scss';
 
 export default function Calendar() {
-  const [month, setMonth] = React.useState(utils.now.getMonth());
 
   return (
     <>
-      <Month month={utils.displayMonth(month)} />
+      <Month />
     </>
   )
 }
