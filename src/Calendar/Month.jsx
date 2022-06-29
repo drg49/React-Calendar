d2923a8c7e1e1fb1999c44855940267672047773
Daniel Gavin <81581925+drg49@users.noreply.github.com>
2022-06-28 20:30:14 -0700

    Adding functionality for changing the year

--- src/Calendar/Month.jsx
+++ src/Calendar/Month.jsx
@@ -2,29 +2,49 @@ import React from 'react';
 import * as utils from '../utils';
 
 export default function Month() {
+  // An integer representing the month of the year
   const [month, setMonth] = React.useState(utils.now.getMonth());
+  // THe current year
+  const [year, setYear] = React.useState(utils.now.getFullYear());
   
-  const increaseMonth = () => month === 11 ? setMonth(0) : setMonth(month + 1);
+  const increaseMonth = () => {
+    if (month === 11) {
+      setYear(year + 1);
+      return setMonth(0);
+    }
 
-  const decreaseMonth = () => month === 0 ? setMonth(11) : setMonth(month - 1);
+    return setMonth(month + 1);
+  }
+
+  const decreaseMonth = () => {
+    if (month === 0) {
+      setYear(year - 1);
+      return setMonth(11);
+    }
+    
+    return setMonth(month - 1);
+  }
 
   return (
-    <div id="month">
-      <button
-        class="btn"
-        onClick={decreaseMonth}
-      >
-        &#10094;
-      </button>
-      <span>
-        {utils.displayMonth(month)}
-      </span>
-      <button
-        class="btn"
-        onClick={increaseMonth}
-      >
-        &#10095;
-      </button>
-    </div>
+    <>
+      <div id="month">
+        <button
+          className="btn"
+          onClick={decreaseMonth}
+        >
+          &#10094;
+        </button>
+        <span>
+          {utils.displayMonth(month)}
+        </span>
+        <button
+          className="btn"
+          onClick={increaseMonth}
+        >
+          &#10095;
+        </button>
+      </div>
+      <span>{year}</span>
+    </>
   )
 }
